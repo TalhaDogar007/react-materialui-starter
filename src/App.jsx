d7c5183732eda0fdf392b392b641af58c93ae878
Your Name <you@example.com>
2022-01-03 14:55:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const App = () => {
+  const classes = useStyles();
+  return <div></div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import App from "./App";
+import theme from "./theme";
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
